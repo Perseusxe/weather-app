697dbad4d8f6e39055ce1317f7942f01fd9804c0
Perseusxe <perseus20061103@gmail.com>
2024-12-25 20:19:22 +0800

    circle

--- src/components/Circles.jsx
+++ src/components/Circles.jsx
@@ -1,8 +1,8 @@
-export const Circles = ()=>{
-    return (<div className="flex items-center justify-center w-full absolute h-full">
-    <div className="w-[140px] h-[140px] absolute bg-transparent border-2 border-slate-500 rounded-full opacity-[10%] z-20"></div>
-        <div className="w-[340px] h-[340px] absolute bg-transparent border-2 border-slate-500 rounded-full opacity-[10%] z-20"></div>
-        <div className="w-[540px] h-[540px] absolute bg-transparent border-2 border-slate-500 rounded-full opacity-[10%] z-20"></div>
-        <div className="w-[940px] h-[940px] absolute bg-transparent border-2 border-slate-500 rounded-full opacity-[10%] z-10"></div>
-    </div>)
-}
+// export const Circles = ()=>{
+//     return (<div className="flex items-center justify-center w-full absolute h-full">
+//     <div className="w-[140px] h-[140px] absolute bg-transparent border-2 border-slate-500 rounded-full opacity-[10%] z-20"></div>
+//         <div className="w-[340px] h-[340px] absolute bg-transparent border-2 border-slate-500 rounded-full opacity-[10%] z-20"></div>
+//         <div className="w-[540px] h-[540px] absolute bg-transparent border-2 border-slate-500 rounded-full opacity-[10%] z-20"></div>
+//         <div className="w-[940px] h-[940px] absolute bg-transparent border-2 border-slate-500 rounded-full opacity-[10%] z-10"></div>
+//     </div>)
+// }
